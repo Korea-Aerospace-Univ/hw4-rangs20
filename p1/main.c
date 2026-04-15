c37a9e8fdd95e3da04de074300ed8cde010b5777
2025050066 <hrhr0107@gmail.com>
2026-04-15 15:42:37 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>						
+
+int main(void)
+{
+	int correct;
+	scanf("%d", &correct);
+	int number;
+	scanf("%d", &number);
+	int i = 1;
+	do
+	{
+		if (number > correct)
+		{
+			printf("%d>?", number);
+		}
+		else if (number < correct)
+		{
+			printf("%d<?", number);
+		}
+		printf("\n");
+		(void)scanf("%d", &number);
+		i++;
+
+	} while (number != correct);
+	if (number == correct) printf("%d==?\n", number);
+	printf("%d",i);
+}
